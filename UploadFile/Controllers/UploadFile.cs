e4893a57cf0ad65289f9eb6f57e69648d7601e3f
sp <far.rahi.3pd@gmail.com>
2023-10-08 17:07:28 +0330

    int

---
+++ UploadFile/Controllers/UploadFile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UploadFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadFile : ControllerBase
+    {
+        [HttpPost(Name = "Upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if(file == null || file?.Length == 0)
+            {
+                return BadRequest("file is invalid!");
+            }
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Uploads", file.FileName);
+            using (var fs = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fs);
+            }
+
+            return Ok("File uploaded");
+        }
+    }
+}
